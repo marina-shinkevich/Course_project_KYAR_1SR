34031f6ea010efcfe5f25aa0181c2cf4dcd21787
Marina Shinkevich <syfvgk@email>
2024-05-01 20:06:41 +0300

    dfghjk

---
+++ scriptGalary.js
@@ -0,0 +1,36 @@
+document.getElementById('ph1').addEventListener('mousemove', function(){
+    this.src = "bmv_2.jpg";
+  })
+  document.getElementById('ph1').addEventListener('mouseout', function(){
+    this.src = "http://cdn.carbuzz.com/gallery-images/1600/553000/400/553469.jpg"
+  })
+  document.getElementById('ph2').addEventListener('mousemove', function(){
+    this.src = "bmv1.jpg";
+  })
+  document.getElementById('ph2').addEventListener('mouseout', function(){
+    this.src = "https://cdn.bmwblog.com/wp-content/uploads/2020/03/The-New-BMW-M760Li-xDrive-G12-6.jpg";
+  })
+  document.getElementById('ph3').addEventListener('mousemove', function(){
+    this.src = "https://kuznitsaspb.ru/wp-content/uploads/9/a/d/9adeffa8d86d309548750d53e524333a.jpeg";
+  })
+  document.getElementById('ph3').addEventListener('mouseout', function(){
+    this.src = "https://ig.drivenn.ru/gg23qoers4r6w_vg64ic.jpeg";
+  })
+  document.getElementById('ph4').addEventListener('mousemove', function(){
+    this.src = "https://motortrivia.com/wp-content/uploads/2021/02/13-audi-e-tron-gt-quattro-and-rs-e-tron-gt-2022.jpg";
+  })
+  document.getElementById('ph4').addEventListener('mouseout', function(){
+    this.src = "https://www.autoindustriya.com/images/posts/post19546.jpg";
+  })
+  document.getElementById('ph5').addEventListener('mousemove', function(){
+    this.src = "https://file.autodoc.ru/news/avto-novosti/854700524_14.jpg";
+  })
+  document.getElementById('ph5').addEventListener('mouseout', function(){
+    this.src = "https://imageio.forbes.com/specials-images/imageserve/647f5dd37839b6c4e657b386/2024-TX-350-Premium-004/0x0.jpg?format=jpg&crop=1264,842,x432,y146,safe&width=960";
+  })
+  document.getElementById('ph6').addEventListener('mousemove', function(){
+    this.src = "https://tmna.aemassets.toyota.com/is/image/toyota/lexus/images/models/gx/2024/gallery/Lexus-GX-gallery-28-desktop-1440x811-LEX-GXG-MY24-0084.01.jpg?wid=1440&hei=811";
+  })
+  document.getElementById('ph6').addEventListener('mouseout', function(){
+    this.src = "https://netcarflix.sfo2.cdn.digitaloceanspaces.com/0000/v3/Lexus/64829a48dd158bb189252bd0/2024-gx-overtrail-lexus-2ac5pi0esepvzkkykk.jpeg";
+  })
